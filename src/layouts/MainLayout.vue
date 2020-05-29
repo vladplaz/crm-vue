a12ba2c3325f65db2a2cf3cd9af6d1d0b4efc45b
Vladyslavvs <57898919+Vladyslavvs@users.noreply.github.com>
2020-05-29 19:00:39 +0300

    finish base leyouts and filters"

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="app-main-layout">
+
+        <NavBar @click="isOpen=!isOpen"/>
+
+        <SideBar v-model="isOpen"/>
+
+        <main class="app-content" :class="{full:!isOpen}">
+            <div class="app-page">
+                <router-view/>
+            </div>
+        </main>
+        <div class="fixed-action-btn">
+            <router-link class="btn-floating btn-large blue" to="/record">
+                <i class="large material-icons">add</i>
+            </router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+    import NavBar from '@/components/app/NavBar'
+    import SideBar from '@/components/app/SideBar'
+    export default {
+        name:'main-layout',
+        data:()=>({
+          isOpen:true
+        }),
+        components:{
+            NavBar,SideBar
+        }
+    }
+</script>
